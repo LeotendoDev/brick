d1241df6517c1e2d3668a53b652da57ff75f2fd9
LeotendoDev <leotendosteinhoff@gmail.com>
2022-04-06 21:01:24 +0200

    Add build system

---
+++ main.c
@@ -0,0 +1,21 @@
+/********************************************
+ *
+ * FILE - main.c
+ *
+ * by Leonard Steinhoff
+ * created at: Do Apr 06 19:28 JST 2022
+ *
+ * licensed under MIT
+ *
+ * Copyright (c) 2021-2022 Leonard Steinhoff
+ *
+ *
+ ********************************************
+*/
+
+int bc_main(int argc, char** argv)
+{
+       
+}
+
+
